205cd0a552f564210ea7fb0c8c53795425ba1756
Shreyansh17glb <25114cn371@glbitm.ac.in>
2026-06-06 18:42:31 +0530

    added day 7 code

---
+++ day_7/recReverse.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int rev = 0;
+void reverseNum(int n) {
+    if (n == 0)
+        return;
+    rev = rev * 10 + n % 10;
+    reverseNum(n / 10);
+}
+int main() {
+    int num;
+    cin >> num;
+    reverseNum(num);
+    cout << rev;
+}
---
+++ day_7/recSumOfDigits.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sumOfDigits(int n) {
+    if (n == 0)
+        return 0;
+    return (n % 10) + sumOfDigits(n / 10);
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Sum of digits = " << sumOfDigits(num);
+    return 0;
+}
