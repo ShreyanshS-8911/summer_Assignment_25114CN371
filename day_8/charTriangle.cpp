96953ea24ee9feb85390652f4f24cc7791048eab
Shreyansh17glb <chikusingh8911@gmail.com>
2026-06-07 19:58:03 +0530

    added day 8 code

---
+++ day_8/charTriangle.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        char ch = 'A';
+        for (int j = 0; j < i; j++) {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
