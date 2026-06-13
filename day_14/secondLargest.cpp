f183305ac7da823323ab57192765241d5a6f566f
Shreyansh17glb <chikusingh8911@gmail.com>
2026-06-13 12:48:05 +0530

    added day 14 code

---
+++ day_14/secondLargest.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int largest = arr[0];
+    int secondLargest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > secondLargest && arr[i] < largest) {
+            secondLargest = arr[i];
+        }
+    }
+    cout << "Second Largest = " << secondLargest;
+    return 0;
+}
