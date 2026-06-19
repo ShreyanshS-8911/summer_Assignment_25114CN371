30eacf175dcb8de903acb04a888893ad607f3ec4
Shreyansh17glb <chikusingh8911@gmail.com>
2026-06-19 12:33:57 +0530

    added day 20 code

---
+++ day_20/symmetricMatrixCheck.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the order of the matrix: ";
+    cin >> n;
+    int A[10][10];
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    bool symmetric = true;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (A[i][j] != A[j][i]) {
+                symmetric = false;
+                break;
+            }
+        }
+        if (!symmetric)
+            break;
+    }
+    if (symmetric)
+        cout << "Matrix is Symmetric";
+    else
+        cout << "Matrix is Not Symmetric";
+    return 0;
+}
